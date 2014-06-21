cdf5e67c7fb6ebeb656cc6f8a6fb66a03b058819
AlexanderC <alexander.moldova@gmail.com>
2014-06-21 10:58:48 +0300

    set deprecated

--- Encryptors/AES256Encryptor.php
+++ Encryptors/AES256Encryptor.php
@@ -6,6 +6,7 @@ namespace VMelnik\DoctrineEncryptBundle\Encryptors;
  * Class for AES256 encryption
  *
  * @author Victor Melnik <melnikvictorl@gmail.com>
+ * @deprecated DO NOT USE THIS EVER!
  */
 class AES256Encryptor implements EncryptorInterface
 {
@@ -25,6 +26,9 @@ class AES256Encryptor implements EncryptorInterface
      */
     public function __construct($key)
     {
+        // TODO: remove this in next release
+        throw new \RuntimeException("This encoder is deprecated");
+
         $this->secretKey = md5($key);
         $this->initializationVector = mcrypt_create_iv(
             mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB),
