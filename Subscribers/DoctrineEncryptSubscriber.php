52128e766b82ba9e638051e6d2b8a292ade8c0c6
Cristi <kow.kristi@gmail.com>
2015-04-04 12:05:25 +0300

    removed unused class

--- Subscribers/DoctrineEncryptSubscriber.php
+++ Subscribers/DoctrineEncryptSubscriber.php
@@ -12,7 +12,6 @@ use \Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Proxy\Proxy;
 use \ReflectionClass;
 use VMelnik\DoctrineEncryptBundle\Encryptors\EncryptorInterface;
-use HRM\MainBundle\Entity\User;
 
 /**
  * Doctrine event subscriber which encrypt/decrypt entities
